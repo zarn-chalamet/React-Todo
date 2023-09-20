de22a09821be861615acc4a2d56d496db6e6d893
zarn_chalamet <6531503195@lamduan.mfu.ac.th>
2023-09-20 15:59:38 +0700

    extract component

---
+++ src/components/TodoFilters.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TodoFilters() {
+  return (
+    <div>
+      <button className="button filter-button filter-button-active">All</button>
+      <button className="button filter-button">Active</button>
+      <button className="button filter-button">Completed</button>
+    </div>
+  );
+}
