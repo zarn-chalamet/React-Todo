99adb88948fec7017dc96934aa6279b7fe411dfc
zarn_chalamet <6531503195@lamduan.mfu.ac.th>
2023-09-20 17:14:34 +0700

    POST method : adding json data into json file

--- src/App.js
+++ src/App.js
@@ -18,11 +18,24 @@ function App() {
       });
   }, []);
 
+  let addTodo = (todo) => {
+    //update data at the server side
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(todo),
+    });
+    //update data at the client side
+    setTodos((prevState) => [...prevState, todo]);
+  };
+
   return (
     <div className="todo-app-container">
       <div className="todo-app">
         <h2>Todo App</h2>
-        <TodoForm />
+        <TodoForm addTodo={addTodo} />
         <TodoList todos={todos} />
         <CheckAllAndRemaining />
         <div className="other-buttons-container">
